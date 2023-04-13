b62dd4f86e711e3b993bf743e340898a2417a399
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-04-12 20:26:56 -0400

    remove unnecessary module

--- afwf_awscli_profile/cache.py
+++ afwf_awscli_profile/cache.py
@@ -4,26 +4,10 @@
 Disk cache for Alfred Workflow.
 """
 
-import typing as T
-from diskcache import Cache
-from pathlib_mate import Path
+from afwf.opt.cache import Cache
 
 from .paths import dir_cache
 
 dir_cache.parent.mkdir(parents=True, exist_ok=True)
 
-
-def decohints(decorator: T.Callable) -> T.Callable:
-    return decorator
-
-
-class TypedCache(Cache):
-    def typed_memoize(self, name=None, typed=False, expire=None, tag=None, ignore=()):
-        @decohints
-        def decorator(func):
-            return self.memoize(name, typed, expire, tag, ignore)(func)
-
-        return decorator
-
-
-cache = TypedCache(dir_cache.abspath)
+cache = Cache(dir_cache.abspath)
